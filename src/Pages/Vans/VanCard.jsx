25d8fbb4e02403a8d90f95f5e9c08d95ad9fb1a1
balazs burian <bur.balazs@protonmail.com>
2025-04-11 14:53:53 +0300

    added van detail page, and some small fixes

--- src/Pages/Vans/VanCard.jsx
+++ src/Pages/Vans/VanCard.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import Button from '../../components/Button'
+import {Link} from 'react-router'
 
 function VanCard({id, name, price, imageUrl, type}) {
 
-  return (
-    <div className='van-card' key={id}>
-      <img src={imageUrl} alt={`van-${name}`}/>
-      <div className="van-card-text-container">
 
-      <div className="van-card-name-btn">
-        <p className="van-card-name">{name}</p>
-        <Button className={`van-card-button-${type}`}>{type}</Button>
+
+
+  return (
+    <div className='van-card'>
+      <Link to={`/vans/${id}`} className='van-card-link'>
+        <img src={imageUrl} alt={`van-${name}`}/>
+        <div className="van-card-text-container">
+
+        <div className="van-card-name-btn">
+          <p className="van-card-name">{name}</p>
+          <Button className={`van-card-button-${type}`}>{type}</Button>
+        </div>
+        <p className="van-card-price">${price}
+          <br />
+          <span>/day</span>
+        </p>
+        </div>
+      </Link>
       </div>
-      <p className="van-card-price">${price}
-        <br />
-        <span>/day</span>
-      </p>
-      </div>
-    </div>
   )
 }
 
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,9 +1,9 @@
 import Home from '../Pages/Home/Home'
 import About from '../Pages/About/About'
 import Vans from '../Pages/Vans/Vans'
+import VanDetail from '../Pages/VanDetail/VanDetail'
 
 
 
 
-
-export {Home, About, Vans};
+export {Home, About, Vans, VanDetail };
